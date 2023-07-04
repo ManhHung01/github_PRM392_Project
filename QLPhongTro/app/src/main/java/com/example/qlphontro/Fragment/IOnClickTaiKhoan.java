6a5f56e6be6642e5f226d2cf90701d1640db75d6
HuyCQ <huycqhe151104@fpt.edu.vn>
2023-07-04 23:52:38 +0700

    Create IOnClickTaiKhoan.java
    Create Interface IOnClickTaiKhoan

---
+++ QLPhongTro/app/src/main/java/com/example/qlphontro/Fragment/IOnClickTaiKhoan.java
@@ -0,0 +1,8 @@
+package com.example.qlphontro.Fragment;
+
+import com.codeandroid.qlphongtro.Model.TaiKhoan;
+import com.example.qlphontro.Model.TaiKhoan;
+
+public interface IOnClickTaiKhoan {
+    void iOnClickTaiKhoan(TaiKhoan taiKhoan, int poss);
+}
