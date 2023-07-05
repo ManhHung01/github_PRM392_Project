993226f32734ce7405429762d60af47215da1395
ManhHung01 <100327995+ManhHung01@users.noreply.github.com>
2023-07-05 23:35:24 +0700

    Update MainActivity.java

--- QLPhongTro/app/src/main/java/com/example/qlphontro/MainActivity.java
+++ QLPhongTro/app/src/main/java/com/example/qlphontro/MainActivity.java
@@ -1,5 +1,18 @@
 package com.example.qlphontro;
 
-public class MainActivity {
-    
-}
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView btnNavi;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        btnNavi = findViewById(R.id.bottomNav);
+        viewPager = findViewById(R.id.viewPager);
+        //btn
+        setupViewPager();
+
+    }
